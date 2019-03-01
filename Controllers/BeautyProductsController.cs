cd1ad06946afc7e9ceac5327ca9f638edcb25a87
ThetHtar23 <48117055+ThetHtar23@users.noreply.github.com>
2019-03-01 13:43:09 +0630

    Initial commit

---
+++ Controllers/BeautyProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BeautyProcutReview;
+
+namespace BeautyProcutReview.Controllers
+{
+    public class BeautyProductsController : Controller
+    {
+        private AssigmentBeautyParaEntities db = new AssigmentBeautyParaEntities();
+
+        // GET: BeautyProducts
+        public ActionResult Index()
+        {
+            return View(db.BeautyProducts.ToList());
+        }
+
+        // GET: BeautyProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BeautyProduct beautyProduct = db.BeautyProducts.Find(id);
+            if (beautyProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beautyProduct);
+        }
+
+        // GET: BeautyProducts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("login", "Account");
+        }
+
+
+        // POST: BeautyProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,image,ProductDescription")] BeautyProduct beautyProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BeautyProducts.Add(beautyProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(beautyProduct);
+        }
+
+        // GET: BeautyProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BeautyProduct beautyProduct = db.BeautyProducts.Find(id);
+            if (beautyProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beautyProduct);
+        }
+
+        // POST: BeautyProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,image,ProductDescription")] BeautyProduct beautyProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(beautyProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(beautyProduct);
+        }
+
+        // GET: BeautyProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BeautyProduct beautyProduct = db.BeautyProducts.Find(id);
+            if (beautyProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beautyProduct);
+        }
+
+        // POST: BeautyProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BeautyProduct beautyProduct = db.BeautyProducts.Find(id);
+            db.BeautyProducts.Remove(beautyProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/MemberReivewsController.cs
@@ -0,0 +1,141 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BeautyProcutReview;
+
+namespace BeautyProcutReview.Controllers
+{
+    public class MemberReivewsController : Controller
+    {
+        private AssigmentBeautyParaEntities db = new AssigmentBeautyParaEntities();
+
+        // GET: MemberReivews
+        public ActionResult Index()
+        {
+            var memberReivews = db.MemberReivews.Include(m => m.AspNetUser).Include(m => m.BeautyProduct);
+            return View(memberReivews.ToList());
+        }
+
+        // GET: MemberReivews/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MemberReivew memberReivew = db.MemberReivews.Find(id);
+            if (memberReivew == null)
+            {
+                return HttpNotFound();
+            }
+            return View(memberReivew);
+        }
+
+        // GET: MemberReivews/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.PrdouctID = new SelectList(db.BeautyProducts, "ProductID", "ProductName");
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return RedirectToAction("Index", "BeautyProducts");
+        }
+
+        // POST: MemberReivews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReviewID,CustomerID,PrdouctID,SuggesstionText")] MemberReivew memberReivew)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MemberReivews.Add(memberReivew);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.AspNetUsers, "Id", "Email", memberReivew.CustomerID);
+            ViewBag.PrdouctID = new SelectList(db.BeautyProducts, "ProductID", "ProductName", memberReivew.PrdouctID);
+            return View(memberReivew);
+        }
+
+        // GET: MemberReivews/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MemberReivew memberReivew = db.MemberReivews.Find(id);
+            if (memberReivew == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.AspNetUsers, "Id", "Email", memberReivew.CustomerID);
+            ViewBag.PrdouctID = new SelectList(db.BeautyProducts, "ProductID", "ProductName", memberReivew.PrdouctID);
+            return View(memberReivew);
+        }
+
+        // POST: MemberReivews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReviewID,CustomerID,PrdouctID,SuggesstionText")] MemberReivew memberReivew)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(memberReivew).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.AspNetUsers, "Id", "Email", memberReivew.CustomerID);
+            ViewBag.PrdouctID = new SelectList(db.BeautyProducts, "ProductID", "ProductName", memberReivew.PrdouctID);
+            return View(memberReivew);
+        }
+
+        // GET: MemberReivews/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MemberReivew memberReivew = db.MemberReivews.Find(id);
+            if (memberReivew == null)
+            {
+                return HttpNotFound();
+            }
+            return View(memberReivew);
+        }
+
+        // POST: MemberReivews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            MemberReivew memberReivew = db.MemberReivews.Find(id);
+            db.MemberReivews.Remove(memberReivew);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
